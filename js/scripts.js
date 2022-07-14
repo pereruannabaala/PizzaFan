879328148902e6c03bd929ec490b3b0c85c211fb
pereruannabaala <pereruannabaala@gmail.com>
2022-07-14 12:01:28 +0300

    add swiper.js cdn

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 // Select the elements
 var toggle_btn;
 var big_wrapper;
+var hamburger_menu;
 
 function declare() {
     toggle_btn = document.querySelector(".toggle-btn");
     big_wrapper = document.querySelector(".big-wrapper");
+    hamburger_menu = document.querySelector(".hamburger-menu");
 }
 
 const main = document.querySelector("main");
@@ -42,6 +44,9 @@ function toggleAnimation() {
 
 function events() {
     toggle_btn.addEventListener("click", toggleAnimation);
+    hamburger_menu.addEventListener("click", () => {
+        big_wrapper.classList.toggle("active");
+    });
 
 }
 
